1d5d9477b7c13b800ff9733c1242bf8a5def8cfc
monumentum-dev <stronamonumentum@gmail.com>
2025-02-20 14:10:15 +0100

    add: first commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sanity(
+    {
+        projectId: 'm66lqwng',
+        dataset: 'production',
+        // Set useCdn to false if you're building statically.
+        useCdn: false,
+      }
+  ), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: netlify()
+});
